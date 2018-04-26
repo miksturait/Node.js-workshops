c621ad6f71e9c9467d23cb52db64de93eef1fccf
Skysplit <manetamajster@gmail.com>
2018-04-26 16:45:27 +0200

    Change EJS to PugJS and move to babel-cli

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 import express from 'express';
-import { renderFile as renderTemplate } from 'ejs';
 import homeRouter from "./home";
 
 const app = express();
 
-app.engine('ejs', renderTemplate);
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 app.use(homeRouter);
 
 export default app;
